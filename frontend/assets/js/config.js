5ecb50dfc7007114afb5cf5d128d950c15a5d7c3
Thành Nhân Phạm <thanhnhan3222@gmail.com>
2025-10-18 16:42:21 +0700

    feat(ui)

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -23,7 +23,7 @@ window.APP_CONFIG = {
     name: 'Snevo',
     version: '1.0.0',
     environment: 'development',
-    buildTime: '2025-10-17T15:02:53.455Z',
+    buildTime: '2025-10-18T08:05:47.068Z',
     features: {
         googleAuth: isValidGoogleClientId && isValidSupabaseUrl && isValidSupabaseKey,
         emailVerification: isValidSupabaseUrl && isValidSupabaseKey,
