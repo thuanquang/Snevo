a5cc8fcd03f40026dfbcaff76f56c692d885ad52
Thành Nhân Phạm <thanhnhan3222@gmail.com>
2025-10-23 08:25:55 +0700

    feat(ui): change the UI of product page

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -23,7 +23,7 @@ window.APP_CONFIG = {
     name: 'Snevo',
     version: '1.0.0',
     environment: 'development',
-    buildTime: '2025-10-21T00:44:22.817Z',
+    buildTime: '2025-10-23T00:58:08.225Z',
     features: {
         googleAuth: isValidGoogleClientId && isValidSupabaseUrl && isValidSupabaseKey,
         emailVerification: isValidSupabaseUrl && isValidSupabaseKey,
