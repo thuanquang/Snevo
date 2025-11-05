0f7046a24d4c9b85c40ed62ab561566ff2b0fd8f
Thành Nhân Phạm <thanhnhan3222@gmail.com>
2025-11-05 20:26:59 +0700

    feat(ui): refractor code

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -23,7 +23,7 @@ window.APP_CONFIG = {
     name: 'Snevo',
     version: '1.0.0',
     environment: 'development',
-    buildTime: '2025-11-04T10:11:45.102Z',
+    buildTime: '2025-11-05T13:10:06.533Z',
     features: {
         googleAuth: isValidGoogleClientId && isValidSupabaseUrl && isValidSupabaseKey,
         emailVerification: isValidSupabaseUrl && isValidSupabaseKey,
