512c40c757238d7e12786aa91bf08e69a3a084b9
zuhon239 <vuxhng2309@gmail.com>
2025-11-06 00:31:35 +0700

    config and upload

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -23,7 +23,7 @@ window.APP_CONFIG = {
     name: 'Snevo',
     version: '1.0.0',
     environment: 'development',
-    buildTime: '2025-11-02T23:06:03.318Z',
+    buildTime: '2025-11-04T17:33:01.566Z',
     features: {
         googleAuth: isValidGoogleClientId && isValidSupabaseUrl && isValidSupabaseKey,
         emailVerification: isValidSupabaseUrl && isValidSupabaseKey,
